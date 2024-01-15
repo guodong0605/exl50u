d22c961ab563266741d515bb8ea71c26497db0ed
GuoDong <990266434@qq.com>
2024-01-15 09:36:48 +0800

    当前给定炮号下，数据库中所有的通道数据

---
+++ common code/downloaddata/chnscan.m
@@ -0,0 +1,21 @@
+function  Chnl=chnscan(shotnum)
+IpAddress='192.168.20.11';
+treeNames={'exl50u'};
+fullPath = mfilename('fullpath');
+[currentPath, ~, ~] = fileparts(fullPath);
+datapath=[currentPath,'\data\exl50u.mat'];
+if exist(datapath, 'file')
+        delete(datapath)
+end
+ myChnlString='';    
+    for i=1:length(treeNames)
+        cmd=['Chnl= getChannelsInTree(''' treeNames{i} ''',' num2str(shotnum) ');'];
+        eval(cmd)
+        Chnl=unique(Chnl);        
+        myChnlString=[myChnlString ':' treeNames{i} ':;'];       
+        cmd='for i=1:length(Chnl); myChnlString=[myChnlString  Chnl{i} '';'' ]; end'; 
+        eval(cmd)
+    end      
+    % save(datapath,'myChnlString')
+    save(datapath,'Chnl')
+end
