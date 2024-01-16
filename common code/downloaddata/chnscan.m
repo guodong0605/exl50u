61f700d4fe6ce0ee462470b0ca06b495e8f8ed55
lijiajia001 <510841834@qq.com>
2024-01-16 18:37:58 +0800

    test4

--- common code/downloaddata/chnscan.m
+++ common code/downloaddata/chnscan.m
@@ -2,6 +2,7 @@ function  Chnl=chnscan(shotnum)
 % This function is used to  edited by lijia 20240116
 % This is a test from guodong PC
 % test 3
+% test4
 IpAddress='192.168.20.11';
 treeNames={'exl50u'};
 fullPath = mfilename('fullpath');
