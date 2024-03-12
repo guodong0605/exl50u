eac69abce3bb7132b466ac1c462d41969edbc783
GuoDong <990266434@qq.com>
2024-03-12 10:31:59 +0800

    flux optimization

---
+++ diagnostics/emd/FluxError.m
@@ -0,0 +1,46 @@
+function [error,TotalFlux]=FluxError(shotnum,fluxCoilName,fluxparam,flatTime)
+coilNamesStr='pf1-10';
+coilNames=extractMultipleStrings(coilNamesStr);  %参与优化的源有哪些磁场；
+fluxparam.fluxCoilName=fluxCoilName;
+fluxparam.X=fluxparam(1);
+fluxparam.Z=fluxparam(2);
+fluxparam.K=fluxparam(3);
+
+dt=0.1;
+fs=1e-2;
+data_shift=1;
+data_start=flatTime-dt/2;
+data_end=flatTime+dt/2;
+datatime=[num2str(data_start),':',num2str(data_end),':',num2str(fs),];
+
+fullPath = mfilename('fullpath');
+[fileDir, ~, ~] = fileparts(fullPath);
+folderpath=[fileDir,'\data\'];
+filepath=[folderpath,'EXL50UProbeFluxLoop.xlsx'];
+CoilParameter=readtable(filepath,'Sheet','coils','VariableNamingRule','preserve');
+%%
+TotalFlux=zeros(47,1);
+for i=1:numel(coilNames)
+    [X2,Y2,FilamentCurrent,GapX]=sourcePrepare(CoilParameter,coilNames{i});    %单个线圈网格化后的一维数据点
+    fluxPF=MMutInductance(fluxparam.X,fluxparam.Z,X2,Y2,FilamentCurrent,GapX);
+    currentName=['i_',coilNames{i}];                                                                        %PF线圈电流通道
+    ipf_data=downloaddata(shotnum,currentName,datatime,0,0);                          %下载PF线圈电流数据
+    pf_flat=mean(ipf_data);                                                                                        %获得PF线圈电流的平均值大小
+    if max(abs(pf_flat))<100
+        flux_compute=zeros(size(TotalFlux,1),1);
+    else
+        flux_compute=fluxPF.*pf_flat;
+    end
+    TotalFlux(:,i)=flux_compute;
+end
+matches = regexp(fluxparam.fluxCoilName, '\d+', 'match');
+numbers = str2double(matches);
+coilName_Flux=TotalFlux(numbers,:);
+coilName_computeFlux=sum(coilName_Flux);
+%%
+Flux_data=downloaddata(shotnum,fluxparam.fluxCoilName,'-3:5:0.01',0,data_shift);                          %下载磁通环数据
+fluxPF_measure=mean(Flux_data(round((data_start+3)*1e2+1):round((data_end+3)*1e2)))*fluxparam.K;                                                    %测量得到的磁通数据
+error = ((fluxPF_measure -coilName_computeFlux)/coilName_computeFlux*100)^2;
+fprintf(['Error=',num2str(error)])
+end
+
--- diagnostics/emd/fluxOpti.m
+++ diagnostics/emd/fluxOpti.m
@@ -1,66 +1,22 @@
-shotnum=1800;
-fluxCoilName='flux023';
-coilNamesStr='pf1-10';
-acq_start=-3;        %采集开始时间，用于去除信号的偏置和漂移
-flatTime=1.5;
-dt=0.1;
-fs=1e-2;
-data_shift=1;
-data_start=flatTime-dt/2;
-data_end=flatTime+dt/2;
-datatime=[num2str(data_start),':',num2str(data_end),':',num2str(fs),];
-fullPath = mfilename('fullpath');
-[fileDir, ~, ~] = fileparts(fullPath);
-folderpath=[fileDir,'\data\'];
-filepath=[folderpath,'EXL50UProbeFluxLoop.xlsx'];
+shotnum=1705;
+fluxCoilName='flux037';
+filepath='E:\git\exl50u\diagnostics\emd\data\EXL50UProbeFluxLoop.xlsx';
 FluxParameter=readtable(filepath,'Sheet','flux','VariableNamingRule','preserve');
-fluxParam.X=table2array(FluxParameter(:,2))/1e3;
-fluxParam.Z=table2array(FluxParameter(:,3))/1e3;
-fluxParam.K=table2array(FluxParameter(:,4));
-%%
-CoilParameter=readtable(filepath,'Sheet','coils','VariableNamingRule','preserve');
-coilNames=extractMultipleStrings(coilNamesStr);
-totalError = 0;
-TotalFlux=zeros(47,1);
-for i=1:numel(coilNames)
-    [X2,Y2,FilamentCurrent,GapX]=sourcePrepare(CoilParameter,coilNames{i});    %单个线圈网格化后的一维数据点
-    fluxPF=MMutInductance(fluxParam.X,fluxParam.Z,X2,Y2,FilamentCurrent,GapX);
-    currentName=['i_',coilNames{i}];                                                                        %PF线圈电流通道
-    ipf_data=downloaddata(shotnum,currentName,datatime,0,0);                          %下载PF线圈电流数据
-    pf_flat=mean(ipf_data);                                                                                        %获得PF线圈电流的平均值大小
-    if max(abs(pf_flat))<100
-        flux_compute=zeros(size(TotalFlux,1),size(TotalFlux,2));
-    else
-        flux_compute=fluxPF.*pf_flat;
-    end    
-    TotalFlux(:,i)=TotalFlux+flux_compute;
-end
 matches = regexp(fluxCoilName, '\d+', 'match');
 numbers = str2double(matches);
-coilName_Flux=TotalFlux(:,numbers);
-%%
-Flux_data=downloaddata(shotnum,fluxCoilName,datatime,0,0);                          %下载磁通环数据
-fluxPF_measure=mean(Flux_data)*fluxParam.K;                                                     %测量得到的磁通数据
-
-error = (fluxPF_measure -TotalFlux).^2;
+fluxParam.X=table2array(FluxParameter(numbers,2))/1e3;
+fluxParam.Z=table2array(FluxParameter(numbers,3))/1e3;
+fluxParam.K=table2array(FluxParameter(numbers,4));
 
 
+initialParams = [fluxParam.X, fluxParam.Z, fluxParam.K];
+lb = [fluxParam.X-0.05, fluxParam.Z-0.05, fluxParam.K-0.02]; % 注意这里的减号修正
+ub = [fluxParam.X+0.05, fluxParam.Z+0.05, fluxParam.K+0.02];
 
+% 调用fmincon
+options = optimoptions('fmincon', 'Algorithm', 'sqp', 'Display', 'iter');
+[optimalParams, fval] = fmincon(@(params)FluxError(shotnum, fluxCoilName,params, 1.8), initialParams, [], [], [], [], lb, ub, [], options);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+% 输出优化结果
+fprintf('优化后的探测器位置和角度：X = %f, Z = %f, Theta = %f\n', optimalParams(1), optimalParams(2), optimalParams(3));
 
